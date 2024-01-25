b1237c634c88cd9317bd499d2c8cbc94fbd1bd8a
t-kupp <kupper.thorge@gmail.com>
2024-01-25 18:25:48 +0100

    various functionalities added

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const randomizeBtn = document.querySelector("#randomizeBtn");
-const divMain = document.querySelector("#main");
+const divTeamDisplay = document.querySelector("#teamDisplay");
+const defaultBtn = document.querySelector("#defaultBtn");
+const freeForAllBtn = document.querySelector("#freeForAllBtn");
+const playerList = document.querySelector("#playerList");
 
 // Function to create new character objects
 function Character(name, category, possibleSlots, imageURL) {
@@ -12,41 +15,78 @@ function Character(name, category, possibleSlots, imageURL) {
 
 // Character database
 const characters = [];
+function addCharactersToDatabase() {
+  new Character("Magnus", "Warrior", [0, 1, 2], "./portraits/magnus.jpg");
+  new Character("Heiner", "Warrior", [0], "./portraits/heiner.jpg");
+  new Character("Grukli", "Warrior", [0, 1, 2], "./portraits/grukli.jpg");
+  new Character("Bree", "Warrior", [0, 1], "./portraits/bree.jpg");
 
-new Character("Magnus", "Warrior", [0, 1, 2], "./portraits/magnus.jpg");
-new Character("Heiner", "Warrior", [0], "./portraits/heiner.jpg");
-new Character("Grukli", "Warrior", [0, 1, 2], "./portraits/grukli.jpg");
-new Character("Bree", "Warrior", [0, 1], "./portraits/bree.jpg");
+  new Character("Andrin", "Scout", [1, 2], "./portraits/andrin.jpg");
+  new Character("Thuls", "Scout", [1, 2], "./portraits/thuls.jpg");
+  new Character("Sylvie", "Scout", [1, 2], "./portraits/sylvie.jpg");
+  new Character("Gustav", "Scout", [1, 2, 3], "./portraits/gustav.jpg");
 
-new Character("Andrin", "Scout", [1, 2], "./portraits/andrin.jpg");
-new Character("Thuls", "Scout", [1, 2], "./portraits/thuls.jpg");
-new Character("Sylvie", "Scout", [1, 2], "./portraits/sylvie.jpg");
-new Character("Gustav", "Scout", [1, 2, 3], "./portraits/gustav.jpg");
+  new Character("Evelyn", "Mage", [1, 2], "./portraits/evelyn.jpg");
+  new Character("Cornelius", "Mage", [1, 2], "./portraits/cornelius.jpg");
+  new Character("Wilbur", "Mage", [1, 2], "./portraits/wilbur.jpg");
+  new Character("Zek", "Mage", [1, 2], "./portraits/zek.jpg");
 
-new Character("Evelyn", "Mage", [1, 2], "./portraits/evelyn.jpg");
-new Character("Cornelius", "Mage", [1, 2], "./portraits/cornelius.jpg");
-new Character("Wilbur", "Mage", [1, 2], "./portraits/wilbur.jpg");
-new Character("Zek", "Mage", [1, 2], "./portraits/zek.jpg");
+  new Character("Reginald", "Healer", [1, 2, 3], "./portraits/reginald.jpg");
+  new Character("Ottis", "Healer", [1, 2, 3], "./portraits/ottis.jpg");
+  new Character("Malukah", "Healer", [1, 2, 3], "./portraits/malukah.jpg");
+  new Character("Nezglekt", "Healer", [1, 2, 3], "./portraits/nezglekt.jpg");
 
-new Character("Reginald", "Healer", [1, 2, 3], "./portraits/reginald.jpg");
-new Character("Ottis", "Healer", [1, 2, 3], "./portraits/ottis.jpg");
-new Character("Malukah", "Healer", [1, 2, 3], "./portraits/malukah.jpg");
-new Character("Nezglekt", "Healer", [1, 2, 3], "./portraits/nezglekt.jpg");
+  new Character("Yogger", "DLC", [0, 1, 2], "./portraits/yogger.jpg");
+  new Character("Laia", "DLC", [1, 2], "./portraits/laia.jpg");
+  new Character("Navalea", "DLC", [1, 2], "./portraits/navalea.jpg");
+  new Character("Amelia", "DLC", [1, 2], "./portraits/amelia.jpg");
+}
+addCharactersToDatabase();
 
-new Character("Yogger", "DLC", [0, 1, 2], "./portraits/yogger.jpg");
-new Character("Laia", "DLC", [1, 2], "./portraits/laia.jpg");
-new Character("Navalea", "DLC", [1, 2], "./portraits/navalea.jpg");
-new Character("Amelia", "DLC", [1, 2], "./portraits/amelia.jpg");
+// Function to create player objects
+function Player(name, bannedChars) {
+  this.name = name;
+  this.bannedChars = bannedChars;
+  players.push(this);
+}
+
+let players = [];
 
 // Randomize team
 let newTeam = [];
+let playerOrder = [];
 
 function randomizeTeam() {
   newTeam.length = 0;
+  playerOrder.length = 0;
+
+  //Player order randomization
+  if (players.length != 0) {
+    for (let i = 0; i < 4; i++) {
+      //make priority list for players that don't have a slot yet
+      let priorityList = [];
+      for (i = 0; i < players.length; i++) {
+        if (!playerOrder.includes(players[i])) {
+          priorityList.push(players[i]);
+        }
+      }
+      //if everyone has a slot, pick a player at random. Otherwise, pick a player who doesn't have a slot
+      let randomPlayer;
+      if (priorityList.length == 0) {
+        randomPlayer = players[Math.floor(Math.random() * players.length)];
+      } else {
+        randomPlayer = priorityList[Math.floor(Math.random() * priorityList.length)];
+      }
+      playerOrder.push(randomPlayer);
+    }
+  }
+
+  //Character randomization
   for (let i = 0; i < 4; i++) {
     let randomCharacter = characters[Math.floor(Math.random() * characters.length)];
     // Check for duplicate character
-    if (newTeam.some((e) => e.name === randomCharacter.name)) {
+    //if (newTeam.some((e) => e.name === randomCharacter.name))
+    if (newTeam.includes(randomCharacter)) {
       i--;
       continue;
     }
@@ -61,10 +101,33 @@ function randomizeTeam() {
 
 // Click the randomize button
 randomizeBtn.addEventListener("click", () => {
-  divMain.textContent = "";
+  divTeamDisplay.textContent = "";
   randomizeTeam();
   for (let i = 0; i < 4; i++) {
-    const teamDisplay = divMain.appendChild(document.createElement("p"));
-    teamDisplay.textContent = newTeam[i].name + " - " + newTeam[i].category;
+    const characterPortrait = divTeamDisplay.appendChild(document.createElement("img"));
+    characterPortrait.src = newTeam[i].imageURL;
+    characterPortrait.classList.add("characterPortrait");
   }
 });
+
+// default button
+defaultBtn.addEventListener("click", () => {
+  characters.length = 0;
+  addCharactersToDatabase();
+});
+
+// free for all button
+freeForAllBtn.addEventListener("click", () => {
+  for (let i = 0; i < characters.length; i++) {
+    characters[i].possibleSlots = [0, 1, 2, 3];
+  }
+});
+
+//Function that lets you find a character by name instead of array index (useful for later(maybe))
+function getCharacterByName(name) {
+  for (let i = 0; i < characters.length; i++) {
+    if (characters[i].name == name) {
+      return characters[i];
+    }
+  }
+}
